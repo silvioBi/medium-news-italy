21cdc4d64ae9e0a6ae6e53a3b1d1e430e95de0b6
SilvioBi <biasiol.silvio@gmail.com>
2018-11-06 22:45:02 +0100

    Simple test crawler to get new italian Medium articles

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const request = require('request')
+const cheerio = require('cheerio')
+const PORT = process.env.PORT || 80
+
+let app = express()
+
+let getNews = callback => {
+    let news = []
+    let url = 'https://medium.com/italia'
+    request(url, function (error, response, html) {
+        if (!error) {
+            let $ = cheerio.load(html)
+            let article = { title: null, link: null }
+            $('h3').filter(function () {
+                // Let's store the data we filter into a variable so we can easily see what's going on.
+                let data = $(this)
+                // In examining the DOM we notice that the title rests within the first child element of the h3 tag. 
+                // Utilizing jQuery we can easily navigate and get the text by writing the following code:
+                article.title = data.children().first().text()
+                // Similar approach is taken for the article link
+                article.link = data.children().first().attr('href')
+                if (!article.link) article.link = data.parent().attr('href')
+                news.push(article)
+            })
+            callback(news)
+        }
+    })
+}
+
+app.get('/', function (req, res) {
+    res.setHeader('Content-Type', 'application/json')
+    let sendCallback = payload => res.send(JSON.stringify(payload))
+    getNews(sendCallback)
+    console.log('📤 Articles sent successfully')
+})
+
+app.listen()
+console.log('🚀 Server started on port 80')
+exports = module.exports = app
